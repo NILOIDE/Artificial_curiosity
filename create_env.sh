0a2bd9a06349390690f74e0c4f79af8c3c049bad
Nil Stolt Ansó <nil@bureaudigiraat.nl>
2020-05-11 17:44:51 +0200

    Fixed test submission file

--- create_env.sh
+++ create_env.sh
@@ -8,10 +8,9 @@ conda create --name AC pytorch torchvision cudatoolkit=10.1 -c pytorch -y
 conda activate AC
 
 pip install --user -r requirements.txt
-pip install pytorch==1.5 -f https://download.pytorch.org/whl/torch_stable.html
 
 # Install atari-py
-pip install --no-index -f https://github.com/Kojoley/atari-py/releases atari_py
+#pip install --no-index -f https://github.com/Kojoley/atari-py/releases atari_py
 
 # Install grid world envs
 cd code/grid-gym
