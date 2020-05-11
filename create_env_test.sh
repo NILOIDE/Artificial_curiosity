8e64d1597ffe0252a0b726ea579c22e2d566d9e4
Nil Stolt Ansó <nil@bureaudigiraat.nl>
2020-05-11 15:04:10 +0200

    Fixed test submission file

--- create_env_test.sh
+++ create_env_test.sh
@@ -6,3 +6,4 @@ module load Anaconda3/2018.12
 conda create --name AC python=3.8 cudatoolkit=10.1 -y
 . /sw/arch/Debian9/EB_production/2019/software/Anaconda3/2018.12/etc/profile.d/conda.sh
 conda activate AC
+conda list
