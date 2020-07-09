012f653a8a3da00a602057992a7b117ead2fd186
Nil Stolt Ansó <nil@bureaudigiraat.nl>
2020-07-09 15:39:51 +0200

    Proper submission files

--- code/submission_files/submit_idf.sh
+++ code/submission_files/submit_idf.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=3-00:00:00
 #SBATCH --mem=20000M
 #SBATCH --nodes=1
-#SBATCH --partition=gpu_short
+#SBATCH --partition=gpu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nil.stoltanso@student.uva.nl
 #SBATCH --output=name%j_idf.out
